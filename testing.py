78ac626ce46d5cfc87f02e9e2219acd9fcbd3c8a
Anticucho <jorge_2jul@hotmail.com>
2022-05-01 04:42:01 -0400

    Clase vision creada y Agregado filtro hsv

--- testing.py
+++ testing.py
@@ -4,21 +4,21 @@ from visionfilter import Vision
 import cv2
 from hsvfilter import HsvFilter
 
-hsv_filter = HsvFilter(0, 136, 0, 179, 66, 255, 0, 24, 255, 0)
+hsv_filter = HsvFilter(0, 0, 192, 179, 66, 255, 0, 25, 255, 0)
 
 
 # capturar la pantalla
 capture = WindowCapture('Musa-Chan - Dofus 2.63.7.8')
 loop_time = time()
-vision = Vision("Img/FresnoResaltadoNegativo3.png")
+vision = Vision("Img/123.png")
 vision.init_control_gui()
 
 
 # loop de captura
 while True:
     img = cv2.imread("Img/screenshot.png")
-    output = vision.apply_hsv_filter(img, hsv_filter)
-    trees = vision.find(img, 0.8)
+    output = vision.apply_hsv_filter(img,hsv_filter)
+    trees = vision.find(img, 0.65)
     output_image = vision.draw_rectangles(img, trees)
     cv2.imshow('output', output_image)
     print('FPS {}'.format(1 / (time() - loop_time)))
